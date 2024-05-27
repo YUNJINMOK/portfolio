2937b9e4cf86d212ba49521c41e6c643dab0df80
zxwgm <aprnaprn114@naver.com>
2024-05-27 09:52:41 +0900

    re

--- src/Component/Footer.jsx
+++ src/Component/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <div className="w-full h-[80px] border-t-2 border-white bg-[#1E2022] flex justify-center items-center">
       <span className="text-3xl font-semibold text-white">
-        © 2024 yun jin mok. All rights reserved
+        Thank you for taking the time to review my portfolio.
       </span>
     </div>
   );
